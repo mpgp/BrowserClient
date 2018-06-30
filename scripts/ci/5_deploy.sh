2a0acafcbfcc14969d47c3ad6fb5b027bbcdae46
Alexander Loktionov <loktionov129@gmail.com>
2018-06-30 12:17:04 +0700

    chore: update package version

--- scripts/ci/5_deploy.sh
+++ scripts/ci/5_deploy.sh
@@ -1,4 +1,8 @@
 COMMANDS_TO_RUN+=('echo 5_deploy.sh ...')
+
+COMMANDS_TO_RUN+=('echo Generate coverage info and deploy it to coveralls.io')
+COMMANDS_TO_RUN+=('cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js')
+
 COMMANDS_TO_RUN+=('npx semantic-release --branch $BRANCH')
 
 COMMANDS_TO_RUN+=('git remote add github https://github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git')
